7960d68e7c6ebb90fb24196e31f8a8fb6ec34d70
Aniket Singh <singh.aniket0111@gmail.com>
2026-01-07 01:32:05 +0530

    Refactor Blockchain class and add user menu
    Refactor blockchain implementation to simplify block creation and add a menu system for user interaction.

--- Blockchain_Implementation.py
+++ Blockchain_Implementation.py
@@ -3,80 +3,89 @@ import time
 
 
 class Block:
-    def __init__(self, index, timestamp, data, previous_hash, nonce=0):
+    def __init__(self, index, data, previous_hash):
         self.index = index
-        self.timestamp = timestamp
+        self.timestamp = time.time()
         self.data = data
         self.previous_hash = previous_hash
-        self.nonce = nonce
+        self.nonce = 0
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        block_contents = (
-            str(self.index)
-            + str(self.timestamp)
-            + str(self.data)
-            + str(self.previous_hash)
-            + str(self.nonce)
-        )
-        return hashlib.sha256(block_contents.encode()).hexdigest()
+        text = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}{self.nonce}"
+        return hashlib.sha256(text.encode()).hexdigest()
 
 
 class Blockchain:
-    def __init__(self, difficulty=2):
-        self.chain = [self.create_genesis_block()]
-        self.difficulty = difficulty
-
-    def create_genesis_block(self):
-        return Block(0, time.time(), "Genesis Block", "0")
-
-    def get_latest_block(self):
-        return self.chain[-1]
+    def __init__(self):
+        self.chain = [Block(0, "Genesis Block", "0")]
 
     def add_block(self, data):
-        previous_block = self.get_latest_block()
-        new_block = Block(
-            index=len(self.chain),
-            timestamp=time.time(),
-            data=data,
-            previous_hash=previous_block.hash
-        )
+        prev = self.chain[-1]
+        new_block = Block(len(self.chain), data, prev.hash)
 
-        self.mine_block(new_block)
+        # Proof of Work (difficulty = 2)
+        while not new_block.hash.startswith("00"):
+            new_block.nonce += 1
+            new_block.hash = new_block.calculate_hash()
+
         self.chain.append(new_block)
-
-    def mine_block(self, block):
-        target = "0" * self.difficulty
-
-        while block.hash[:self.difficulty] != target:
-            block.nonce += 1
-            block.hash = block.calculate_hash()
 
     def is_valid(self):
         for i in range(1, len(self.chain)):
-            current = self.chain[i]
-            previous = self.chain[i - 1]
+            cur = self.chain[i]
+            prev = self.chain[i - 1]
 
-            # Recalculate hash
-            if current.hash != current.calculate_hash():
+            if cur.hash != cur.calculate_hash():
                 return False
 
-            # Check hash linking
-            if current.previous_hash != previous.hash:
+            if cur.previous_hash != prev.hash:
                 return False
 
         return True
 
+    def show_chain(self):
+        for block in self.chain:
+            print("\n------------------------")
+            print("Index:", block.index)
+            print("Timestamp:", block.timestamp)
+            print("Data:", block.data)
+            print("Previous Hash:", block.previous_hash)
+            print("Hash:", block.hash)
+            print("Nonce:", block.nonce)
+        print("------------------------\n")
 
 
-if __name__ == "__main__":                  # Example usage
-    blockchain = Blockchain(difficulty=2)
+# -------- MENU SYSTEM --------
 
-    blockchain.add_block("First transaction")
-    blockchain.add_block("Second transaction")
-    blockchain.add_block("Third transaction")
+bc = Blockchain()
 
-    print("Blockchain valid:", blockchain.is_valid())
+while True:
+    print("\n====== SIMPLE BLOCKCHAIN MENU ======")
+    print("1. Add new block")
+    print("2. View blockchain")
+    print("3. Check blockchain validity")
+    print("4. Exit")
 
-    for block in blockchain.chain:
-        print(vars(block))
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        data = input("Enter data for new block: ")
+        bc.add_block(data)
+        print(" Block added successfully!")
+
+    elif choice == "2":
+        bc.show_chain()
+
+    elif choice == "3":
+        if bc.is_valid():
+            print(" Blockchain is valid.")
+        else:
+            print(" Blockchain has been tampered!")
+
+    elif choice == "4":
+        print("Exiting program...")
+        break
+
+    else:
+        print(" Invalid choice. Try again.")
